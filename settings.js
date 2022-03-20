f1140a46d4379022cdbede0b074143806bdf3a83
d281123 <98954476+d281123@users.noreply.github.com>
2022-03-20 20:47:31 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -12,27 +12,27 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
+global.autoread = true // auto read pesan / message
+global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
+global.available = true //status online (online)
 
 // Other
-global.botname = "Yuki Adisti" //namabot kalian
-global.ownername = "ᴹᴿ᭄ King Of Bear ×፝֟͜×" //nama kalian
-global.myweb ="https://apikey-bear3.herokuapp.com" //bebas asal jan hapus
-global.youtube = "https://www.youtube.com/channel/UCVSEzfCh6VYL3g9ixHnSqXQ" //bebas asal jan hapus
+global.botname = "RUSHBOTZ" //namabot kalian
+global.ownername = "Dhani" //nama kalian
+global.myweb ="www.instagram.com/dhani.2811" //bebas asal jan hapus
+global.youtube = "https://vt.tiktok.com/ZSd82QaoD/" //bebas asal jan hapus
 global.github = "https://saipulanuar.github.io/" //bebas
-global.email = "drakipul1016@gmail.com" //bebas
+global.email = "aryadani744@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6288279268363" // nomor wa kalian
-global.ownernomerr = "+6288279268363" //nmr wa kalian
+global.ownernomer = "6283112170548" // nomor wa kalian
+global.ownernomerr = "+6283112170548" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6288279268363","6288279268363","6288279268363"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© YukiBot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh KingOfBear' //sticker wm ganti nama kalian
+global.owner = ["6283112170548"] //ganti agar fitur owner bisa di gunakan
+global.packname = '+62 877-4331-3291' //sticker wm ubah
+global.author = 'RushBotz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
